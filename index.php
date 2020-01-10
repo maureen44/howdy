019baad04cb17a7f0fedabad2138ba6aad2e76da
Maureen Kariuki <mkariuki2@mail.greenriver.edu>
2020-01-10 13:41:25 -0500

    Added an index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//This is our Controller!
+//Turn on error reporting -- this is critical
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+include_once('vendor/autoload.php');
+
+//Create an instance of the base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo 'Howdy!';
+});
+
+//Run fat free
+$f3->run();
